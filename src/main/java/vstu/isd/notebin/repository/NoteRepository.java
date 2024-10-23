8392301b7bcb5343e7788a0b2ae1bf445d5f82c2
KachanVlg <vlad.bend@inbox.ru>
2024-10-24 02:01:53 +0300

    feat: created interface NoteRepository

--- src/main/java/vstu/isd/notebin/repository/NoteRepository.java
+++ src/main/java/vstu/isd/notebin/repository/NoteRepository.java
@@ -1,6 +1,14 @@
 package vstu.isd.notebin.repository;
 
 
-public interface NoteRepository {
+import vstu.isd.notebin.entities.Note;
 
+public interface NoteRepository {
+    Iterable<Note> findAll();
+    Note findById(int id);
+    void save(Note note);
+    void delete(Note note);
+    Note findByUrl(String url);
+    void update(Note note);
+    Iterable<Note> findByAvailable(Boolean available);
 }
