4043a26140613a8b5fbd02007aa5f2449f18d234
sauce-chili <saucechilidev@gmail.com>
2024-11-30 03:14:11 +0300

    feat: added method for selective update

--- src/main/java/vstu/isd/notebin/repository/NoteRepository.java
+++ src/main/java/vstu/isd/notebin/repository/NoteRepository.java
@@ -1,14 +1,39 @@
 package vstu.isd.notebin.repository;
+
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
-import vstu.isd.notebin.entities.Note;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import vstu.isd.notebin.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
-import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.function.UnaryOperator;
 
 @Repository
-public interface NoteRepository extends JpaRepository<Note, Long>{
+public interface NoteRepository extends JpaRepository<Note, Long> {
     Optional<Note> findByUrl(String url);
+
     void deleteByUrl(String url);
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    default Note updateWithLock(String url, UnaryOperator<Note> modifier) {
+        return __update(url, true, modifier);
+    }
+
+    private Note __update(String url, boolean updateIfUnmodified, UnaryOperator<Note> modifier) {
+
+        Note note = findByUrl(url).orElseThrow(NoSuchElementException::new);
+
+        Note updated = modifier.apply(note);
+
+        if (updateIfUnmodified || !note.equals(updated)) {
+            return save(updated);
+        }
+
+        return updated;
+    }
 }
