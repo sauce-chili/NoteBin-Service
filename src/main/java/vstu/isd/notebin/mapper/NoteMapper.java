d744cb62f849e07ee7f52183be6d7a0af30b38b2
sauce-chili <saucechilidev@gmail.com>
2024-11-30 03:19:12 +0300

    feat: added note mapper

---
+++ src/main/java/vstu/isd/notebin/mapper/NoteMapper.java
@@ -0,0 +1,42 @@
+package vstu.isd.notebin.mapper;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import vstu.isd.notebin.dto.GetNoteResponseDto;
+import vstu.isd.notebin.dto.NoteDto;
+import vstu.isd.notebin.dto.UpdateNoteRequestDto;
+import vstu.isd.notebin.entity.ExpirationType;
+import vstu.isd.notebin.entity.Note;
+import vstu.isd.notebin.entity.BaseNote;
+import vstu.isd.notebin.entity.NoteCacheable;
+
+import java.time.LocalDateTime;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.FIELD,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE
+)
+public interface NoteMapper {
+
+    @Mapping(source = "available", target = "isAvailable")
+    NoteDto toDto(NoteCacheable noteCacheable);
+
+    @Mapping(source = "available", target = "isAvailable")
+    NoteDto toDto(Note note);
+
+    @Mapping(source = "available", target = "isAvailable")
+    Note toEntity(NoteCacheable noteCacheable);
+
+    @Mapping(source = "available", target = "isAvailable")
+    NoteCacheable toCacheable(Note note);
+
+    @Mapping(source = "available", target = "isAvailable")
+    NoteCacheable toCacheable(NoteDto noteDto);
+
+    @Mapping(source = "available", target = "isAvailable")
+    GetNoteResponseDto toGetNoteResponseDto(NoteDto note);
+}
+
