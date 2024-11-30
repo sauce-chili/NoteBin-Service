960ebca7d389c75585a5ac04e4d3c37bfb3b5523
sauce-chili <saucechilidev@gmail.com>
2024-11-30 03:36:26 +0300

    feat: added `UpdateNoteRequestDto`

---
+++ src/main/java/vstu/isd/notebin/dto/UpdateNoteRequestDto.java
@@ -0,0 +1,21 @@
+package vstu.isd.notebin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vstu.isd.notebin.entity.ExpirationType;
+
+import java.time.Duration;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class UpdateNoteRequestDto {
+    private String title;
+    private String content;
+    private ExpirationType expirationType;
+    private Duration expirationPeriod;
+    private Boolean isAvailable;
+}
