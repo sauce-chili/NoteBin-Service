b1f87c4990ddff885cc3c3628597b419514ab049
Vacobi <roman.yurasov.v@gmail.com>
2024-12-17 23:26:41 +0300

    Created dummy NoteServiceValidator

---
+++ src/main/java/vstu/isd/notebin/service/NoteServiceValidator.java
@@ -0,0 +1,12 @@
+package vstu.isd.notebin.service;
+
+import java.util.Optional;
+
+public class NoteServiceValidator {
+
+    public Optional<ValidationGroupException> validateCreateNoteRequest(){
+
+        // TODO обговорить формат ошибок
+        return Optional.empty();
+    }
+}
