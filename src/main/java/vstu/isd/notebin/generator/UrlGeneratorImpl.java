79affd1112a147a2213a4039dbfda3b79f18501b
Vacobi <roman.yurasov.v@gmail.com>
2024-12-20 00:01:44 +0300

    feat: added usage 'HashCache' in URL generator

--- src/main/java/vstu/isd/notebin/generator/UrlGeneratorImpl.java
+++ src/main/java/vstu/isd/notebin/generator/UrlGeneratorImpl.java
@@ -1,12 +1,19 @@
 package vstu.isd.notebin.generator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import vstu.isd.notebin.cache.HashCache;
+import vstu.isd.notebin.entity.Hash;
 
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class UrlGeneratorImpl implements UrlGenerator {
+
+    private final HashCache hashCache;
+
     @Override
     public String generateUrl() {
-        return UUID.randomUUID().toString();
+        return hashCache.getHash();
     }
 }
