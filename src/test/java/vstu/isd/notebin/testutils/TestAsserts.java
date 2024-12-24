7729726ccfa3169ce3a46685582d041602d4989e
Vacobi <roman.yurasov.v@gmail.com>
2024-12-24 18:29:48 +0300

    feat: added equals assert for two NoteDto with time error for fields 'createdAt' and 'expirationFrom'

--- src/test/java/vstu/isd/notebin/testutils/TestAsserts.java
+++ src/test/java/vstu/isd/notebin/testutils/TestAsserts.java
@@ -20,6 +20,24 @@ public class TestAsserts {
         assertDurationEquals(expected.getExpirationPeriod(), actual.getExpirationPeriod());
     }
 
+    public static void assertNoteDtoEqualsWithTimeError(NoteDto expected, NoteDto actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getUrl(), actual.getUrl());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getContent(), actual.getContent());
+        assertEquals(expected.isAvailable(), actual.isAvailable());
+        assertEquals(expected.getExpirationType(), actual.getExpirationType());
+        assertDateTimeEqualsNowWithOneSecondError(expected.getCreatedAt(), actual.getCreatedAt());
+        assertDateTimeEqualsNowWithOneSecondError(expected.getExpirationFrom(), actual.getExpirationFrom());
+        assertDurationEquals(expected.getExpirationPeriod(), actual.getExpirationPeriod());
+    }
+
+    public static void assertDateTimeEqualsNowWithOneSecondError(LocalDateTime expected, LocalDateTime actual) {
+        Duration difference = Duration.between(expected, actual);
+
+        assertEquals(0, difference.getSeconds());
+    }
+
     private static void assertLocalDateTimeEquals(LocalDateTime expected, LocalDateTime actual) {
         if (expected != null && actual != null) {
             assertTrue(Duration.between(expected, actual).getSeconds() < 1);
