cade80d2c90e2d4c53a24d2155a3aba1159de0c7
Vacobi <roman.yurasov.v@gmail.com>
2024-12-24 18:34:34 +0300

    refactoring: added @ResponseBody annotation to 'createNote' method in controller for return value

--- src/main/java/vstu/isd/notebin/controller/NoteController.java
+++ src/main/java/vstu/isd/notebin/controller/NoteController.java
@@ -23,7 +23,7 @@ public class NoteController {
     }
 
     @PostMapping
-    public GetNoteResponseDto createNote(@RequestBody CreateNoteRequestDto requestDto) {
+    public @ResponseBody GetNoteResponseDto createNote(@RequestBody CreateNoteRequestDto requestDto) {
         NoteDto noteDto = noteService.createNote(requestDto);
 
         return noteMapper.toGetNoteResponseDto(noteDto);
