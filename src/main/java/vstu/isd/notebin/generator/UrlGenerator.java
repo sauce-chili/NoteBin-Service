735281b45297d440df59dfb0b63418e1b7732f28
Vacobi <roman.yurasov.v@gmail.com>
2024-12-24 22:21:09 +0300

    refactoring: return value of 'generateUrls' method changed to List

--- src/main/java/vstu/isd/notebin/generator/UrlGenerator.java
+++ src/main/java/vstu/isd/notebin/generator/UrlGenerator.java
@@ -1,10 +1,11 @@
 package vstu.isd.notebin.generator;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface UrlGenerator {
 
-    Stream<String> generateUrls(int amount);
+    List<String> generateUrls(int amount);
 
     String generateUrl();
 }
