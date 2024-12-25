3cbc73da3e47e243f6395de8b2e37b5bd70476ff
Vacobi <roman.yurasov.v@gmail.com>
2024-12-25 22:58:49 +0300

    refactor: removed direct dependency from NoteConfig class

--- src/main/java/vstu/isd/notebin/cache/NoteCacheHeater.java
+++ src/main/java/vstu/isd/notebin/cache/NoteCacheHeater.java
@@ -1,9 +1,11 @@
 package vstu.isd.notebin.cache;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import vstu.isd.notebin.config.NoteConfig;
 import vstu.isd.notebin.entity.Note;
@@ -11,18 +13,27 @@ import vstu.isd.notebin.entity.NoteCacheable;
 import vstu.isd.notebin.mapper.NoteMapper;
 import vstu.isd.notebin.repository.NoteRepository;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-@RequiredArgsConstructor
-@EnableConfigurationProperties(NoteConfig.class)
 public class NoteCacheHeater {
 
     private final NoteRepository noteRepository;
     private final NoteMapper noteMapper;
-    private final NoteConfig noteConfig;
+    private final int PAGE_SIZE;
+
+    NoteCacheHeater(
+            NoteRepository noteRepository,
+            NoteMapper noteMapper,
+            int pageSize
+    ) {
+        this.noteRepository = noteRepository;
+        this.noteMapper = noteMapper;
+        PAGE_SIZE = pageSize;
+    }
 
     public List<NoteCacheable> getMostUsedNotes(int amount) {
         List<Note> mostUsedNotes = new ArrayList<>();
@@ -31,7 +42,7 @@ public class NoteCacheHeater {
         int pageIndex = 0;
         boolean repositoryContainsNotes = true;
         while (loaded < amount && repositoryContainsNotes) {
-            Page<Note> notePage = noteRepository.findAll(PageRequest.of(pageIndex, noteConfig.pageSize()));
+            Page<Note> notePage = noteRepository.findAll(PageRequest.of(pageIndex, PAGE_SIZE));
 
             if (notePage.hasContent()){
                 mostUsedNotes.addAll(notePage.stream().toList());
