d570c43ee619e9fbe0fbb7759928c2bfc32d3042
Vacobi <roman.yurasov.v@gmail.com>
2025-01-03 00:38:34 +0300

    refactor: 'getMostUsedNotes' implementation changed to iterable

--- src/main/java/vstu/isd/notebin/cache/NoteCacheHeater.java
+++ src/main/java/vstu/isd/notebin/cache/NoteCacheHeater.java
@@ -1,21 +1,15 @@
 package vstu.isd.notebin.cache;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-import vstu.isd.notebin.config.NoteConfig;
 import vstu.isd.notebin.entity.Note;
 import vstu.isd.notebin.entity.NoteCacheable;
 import vstu.isd.notebin.mapper.NoteMapper;
 import vstu.isd.notebin.repository.NoteRepository;
 
-import java.time.Duration;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,21 +31,22 @@ public class NoteCacheHeater {
         PAGE_SIZE = pageSize;
     }
 
+    private final static Pageable EMPTY_PAGEABLE = Pageable.unpaged();
+
     public List<NoteCacheable> getMostUsedNotes(int amount) {
+
         List<Note> mostUsedNotes = new LinkedList<>();
 
         final int INDEX_OF_FIRST_PAGE = 0;
-        int totalPages = noteRepository.findAll(PageRequest.of(INDEX_OF_FIRST_PAGE, PAGE_SIZE)).getTotalPages();
+        Pageable pageable = PageRequest.of(INDEX_OF_FIRST_PAGE, PAGE_SIZE);
+        int remaining = amount;
+        while(remaining > 0 && pageable != EMPTY_PAGEABLE){
 
-        int loaded = 0;
-        int currentPageIndex = 0;
-        while (nextPageExists(currentPageIndex, totalPages) && notEnoughLoaded(loaded, amount)) {
+            Page<Note> notePage = noteRepository.findAll(pageable);
+            mostUsedNotes.addAll(necessaryNotesOnPage(notePage, remaining));
 
-            Page<Note> notePage = noteRepository.findAll(PageRequest.of(currentPageIndex, PAGE_SIZE));
-
-            mostUsedNotes.addAll(notePage.stream().toList());
-            loaded += notePage.getTotalElements();
-            currentPageIndex++;
+            remaining = amount - mostUsedNotes.size();
+            pageable = nextPageable(notePage, pageable);
         }
 
         return mostUsedNotes.stream()
@@ -59,11 +54,28 @@ public class NoteCacheHeater {
                 .collect(Collectors.toList());
     }
 
-    private boolean nextPageExists(int currentPageIndex, int totalPages){
-        return currentPageIndex + 1 <= totalPages;
+    private List<Note> necessaryNotesOnPage(Page<Note> notePage, int necessary) {
+
+        List<Note> currentNotesOnPage = notePage.getContent();
+        List<Note> necessaryNotes = currentNotesOnPage.subList(
+                0,
+                countOfNecessaryNotesOnPage(necessary, currentNotesOnPage)
+        );
+
+        return necessaryNotes;
     }
 
-    private boolean notEnoughLoaded(int loaded, int required) {
-        return loaded < required;
+    private int countOfNecessaryNotesOnPage(int remaining, List<Note> currentNotesOnPage) {
+
+        return Math.min(remaining, currentNotesOnPage.size());
+    }
+
+    private Pageable nextPageable(Page page, Pageable pageable) {
+
+        if(page.hasNext()){
+            return pageable.next();
+        }
+
+        return EMPTY_PAGEABLE;
     }
 }
