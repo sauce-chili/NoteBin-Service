957a4608d4235e4713e9c9aba8da2a9261102142
sauce-chili <saucechilidev@gmail.com>
2025-01-03 06:34:48 +0300

    fix: remove currently not exists repo

--- src/main/java/vstu/isd/notebin/cache/NoteCacheHeater.java
+++ src/main/java/vstu/isd/notebin/cache/NoteCacheHeater.java
@@ -1,17 +1,15 @@
 package vstu.isd.notebin.cache;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import vstu.isd.notebin.entity.NoteCacheable;
-import vstu.isd.notebin.repository.NoteRepository;
+
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class NoteCacheHeater {
-    private final NoteRepository noteRepository;
 
     public List<NoteCacheable> getMostUsedNotes(int amount) {
         return List.of(); // TODO implement
