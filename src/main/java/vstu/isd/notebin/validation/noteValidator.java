8f5f74e0760269d2170da11f92a227cfa691a336
Vacobi <roman.yurasov.v@gmail.com>
2025-01-04 19:40:49 +0300

    feat: added validation rules from conf file

--- src/main/java/vstu/isd/notebin/validation/noteValidator.java
+++ src/main/java/vstu/isd/notebin/validation/noteValidator.java
@@ -1,12 +1,32 @@
 package vstu.isd.notebin.validation;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import vstu.isd.notebin.dto.CreateNoteRequestDto;
+import vstu.isd.notebin.exception.BaseClientException;
+import vstu.isd.notebin.exception.ClientExpInfo;
 import vstu.isd.notebin.exception.ValidationGroupException;
 
 import java.util.Optional;
 
 public class noteValidator {
 
-    public static Optional<ValidationGroupException> validateCreateNoteRequest(){
+    private final String titleRegexp;
+    private final String contentRegexp;
+    private final int titleLength;
+
+    public noteValidator(
+            @Qualifier("titleRegexp") String titleRegexp,
+            @Qualifier("contentRegexp") String contentRegexp,
+            @Qualifier("titleLength") int titleLength
+    ) {
+        this.titleRegexp = titleRegexp;
+        this.contentRegexp = contentRegexp;
+        this.titleLength = titleLength;
+    }
+
+    public static Optional<ValidationGroupException> validateCreateNoteRequestDto(CreateNoteRequestDto createNoteRequestDto){
+
+        List<BaseClientException> exceptions = new ArrayList<>();
 
         // TODO обговорить формат ошибок
         return Optional.empty();
