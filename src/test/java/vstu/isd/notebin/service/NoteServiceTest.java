394958bfa824c4cb2a4d84d249c77f49e1f0e987
Vacobi <roman.yurasov.v@gmail.com>
2025-01-07 19:17:57 +0300

    test: removed unused annotation

--- src/test/java/vstu/isd/notebin/service/NoteServiceTest.java
+++ src/test/java/vstu/isd/notebin/service/NoteServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import vstu.isd.notebin.cache.NoteCache;
 import vstu.isd.notebin.config.TestContainersConfig;
