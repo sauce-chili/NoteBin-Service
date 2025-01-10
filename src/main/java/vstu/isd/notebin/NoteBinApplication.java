bc022fb2698e3754efdc9242766e997f597bd124
sauce-chili <saucechilidev@gmail.com>
2025-01-11 01:36:25 +0300

    feat: added usage annotation for using feign client

--- src/main/java/vstu/isd/notebin/NoteBinApplication.java
+++ src/main/java/vstu/isd/notebin/NoteBinApplication.java
@@ -1,15 +1,15 @@
 package vstu.isd.notebin;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 @EnableRetry
 public class NoteBinApplication {
     public static void main(String[] args) {
