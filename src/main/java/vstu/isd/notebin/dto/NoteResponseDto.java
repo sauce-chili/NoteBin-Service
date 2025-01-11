8f58ffb90651ccce7c0ef91bbc5fa35576f4663b
sauce-chili <saucechilidev@gmail.com>
2025-01-11 19:26:53 +0300

    feat: added url in `NoteResponseDto`

--- src/main/java/vstu/isd/notebin/dto/NoteResponseDto.java
+++ src/main/java/vstu/isd/notebin/dto/NoteResponseDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class NoteResponseDto {
+    private String url;
     private String title;
     private String content;
     private LocalDateTime createdAt;
