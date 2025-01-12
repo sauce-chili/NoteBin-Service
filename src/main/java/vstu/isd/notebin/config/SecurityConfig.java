805f42199c05be9b63e5915e3bdc84b5d1ff0fbd
sauce-chili <saucechilidev@gmail.com>
2025-01-12 03:51:50 +0300

    feat: implemented `SecurityConfig` with url protection

--- src/main/java/vstu/isd/notebin/config/SecurityConfig.java
+++ src/main/java/vstu/isd/notebin/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package vstu.isd.notebin.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import vstu.isd.notebin.controller.filter.JwtAuthFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable() // TODO fix this
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/api/v1/note").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/note/*").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
