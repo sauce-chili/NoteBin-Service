d09a87c4db9811030111541e8deaa3fd47c795af
Vacobi <roman.yurasov.v@gmail.com>
2025-01-15 02:10:34 +0300

    feat: added method in 'ViewNoteRepository' allows to find value by id

--- src/main/java/vstu/isd/notebin/repository/ViewNoteRepository.java
+++ src/main/java/vstu/isd/notebin/repository/ViewNoteRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vstu.isd.notebin.entity.ViewNote;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface ViewNoteRepository extends JpaRepository<ViewNote, Long> {
 
     Set<ViewNote> findByNoteId(Long noteId);
+
+    Optional<ViewNote> findById(Long id);
 }
