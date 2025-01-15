2ed408ffb0064977988016ede5a8c7506daf4bbe
Vacobi <roman.yurasov.v@gmail.com>
2025-01-15 19:53:42 +0300

    feat: added methods for count of views from authorized and not authorized users

--- src/main/java/vstu/isd/notebin/repository/ViewNoteRepository.java
+++ src/main/java/vstu/isd/notebin/repository/ViewNoteRepository.java
@@ -1,16 +1,24 @@
 package vstu.isd.notebin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vstu.isd.notebin.entity.ViewNote;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public interface ViewNoteRepository extends JpaRepository<ViewNote, Long> {
 
-    Set<ViewNote> findByNoteId(Long noteId);
-
     Optional<ViewNote> findById(Long id);
+
+    @Query(value = "SELECT * FROM view_note WHERE note_id = :noteId AND user_id = :userId LIMIT 1", nativeQuery = true)
+    Optional<ViewNote> findByNoteIdAndUserId(@Param("noteId") Long noteId, @Param("userId") Long userId);
+
+    @Query("SELECT COUNT(*) FROM ViewNote v WHERE v.noteId = :noteId AND v.userId IS NULL")
+    Long countOfNonAuthorizedViews(@Param("noteId") Long noteId);
+
+    @Query("SELECT COUNT(*) FROM ViewNote v WHERE v.noteId = :noteId AND v.userId IS NOT NULL")
+    Long countOfAuthorizedViews(@Param("noteId") Long noteId);
 }
