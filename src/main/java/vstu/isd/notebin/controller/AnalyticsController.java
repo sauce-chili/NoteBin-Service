c066111699d32c556895e35b91622a2726c02d4c
sauce-chili <saucechilidev@gmail.com>
2025-01-16 05:06:10 +0300

    fix: change end-point interface

--- src/main/java/vstu/isd/notebin/controller/AnalyticsController.java
+++ src/main/java/vstu/isd/notebin/controller/AnalyticsController.java
@@ -2,15 +2,10 @@ package vstu.isd.notebin.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import vstu.isd.notebin.dto.GetUserNotesRequestDto;
-import vstu.isd.notebin.dto.GetUserNotesResponseDto;
-import vstu.isd.notebin.dto.NoteDto;
-import vstu.isd.notebin.dto.ViewAnalyticsDto;
+import vstu.isd.notebin.dto.*;
 import vstu.isd.notebin.mapper.NoteMapper;
 import vstu.isd.notebin.service.AnalyticsService;
-import vstu.isd.notebin.service.NoteService;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -20,23 +15,15 @@ import java.util.Optional;
 public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
-    private final NoteService noteService;
     private final NoteMapper noteMapper;
 
-    @GetMapping("/view-notes/me")
-    public Map<String, ViewAnalyticsDto> getMyNotesAnalytics(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestAttribute("x-user-id") Long userId
+    @GetMapping("/view-notes")
+    public Map<String, ViewAnalyticsDto> getViewNotesAnalytics(
+            @RequestBody GetViewsNoteRequestDto requestViews
     ) {
-        GetUserNotesResponseDto<NoteDto> userNotesDto = noteService.getUserNotes(
-                new GetUserNotesRequestDto(userId, page)
+        Map<String, Optional<ViewAnalyticsDto>> viewAnalyticsOptional = analyticsService.getNotesViewAnalytics(
+                requestViews.getUrls()
         );
-
-        List<NoteDto> notes = userNotesDto.getPage().getContent();
-        List<String> urls = notes.stream().map(NoteDto::getUrl).toList();
-
-        Map<String, Optional<ViewAnalyticsDto>> viewAnalyticsOptional = analyticsService.getNotesViewAnalytics(urls);
-
         return noteMapper.toMapStringToViewNote(viewAnalyticsOptional);
     }
 }
