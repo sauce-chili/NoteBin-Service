1f0fb8aae5618739aed146b6b9b6c8e809b87f84
sauce-chili <saucechilidev@gmail.com>
2025-01-17 21:11:08 +0300

    feat: moved url of `auth-service` to env props

--- src/main/java/vstu/isd/notebin/api/auth/AuthApi.java
+++ src/main/java/vstu/isd/notebin/api/auth/AuthApi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 // TODO move url in properties
-@FeignClient(name = "auth-service", url = "http://localhost:8081/api/v1/auth")
+@FeignClient(name = "auth-service", url = "${spring.feign-clients.auth.url}")
 public interface AuthApi {
     @PostMapping("/verify-access")
     Boolean verifyAccessToken(@Valid @RequestBody VerifyAccessTokenRequest request);
