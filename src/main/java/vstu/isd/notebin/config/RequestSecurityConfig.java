184ae775de8027a0c2397bf7f519239a51e28127
sauce-chili <saucechilidev@gmail.com>
2025-02-01 14:19:06 +0300

    feat: injected cors config

--- src/main/java/vstu/isd/notebin/config/RequestSecurityConfig.java
+++ src/main/java/vstu/isd/notebin/config/RequestSecurityConfig.java
@@ -8,55 +8,33 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 import vstu.isd.notebin.controller.filter.JwtAuthFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class RequestSecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.cors().configurationSource(corsConfigurationSource()) // ✅ Включаем CORS
+        http.cors().configurationSource(corsConfigurationSource) // ✅ Включаем CORS
                 .and()
                 .csrf().disable() // TODO fix this
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers(HttpMethod.POST, "/api/v1/note").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/note/{url}").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/analytics/view-notes").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/note/list/me").authenticated() // ✅ Разрешаем доступ авторизованным
+                        .requestMatchers(HttpMethod.GET, "/api/v1/note/{url}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/note/list/me").authenticated()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        return new CorsFilter(corsConfigurationSource());
-    }
-
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://localhost:8081"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setExposedHeaders(List.of("Authorization", "Content-Length"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
     }
 
     @Bean
