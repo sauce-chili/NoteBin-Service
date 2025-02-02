97aa86c366cfeba84d57c9c7bdd7144839a75033
sauce-chili <saucechilidev@gmail.com>
2025-02-02 03:58:55 +0300

    feat: added usage common utils function

--- src/test/java/vstu/isd/notebin/testutils/TestAsserts.java
+++ src/test/java/vstu/isd/notebin/testutils/TestAsserts.java
@@ -1,14 +1,18 @@
 package vstu.isd.notebin.testutils;
 
 import vstu.isd.notebin.dto.NoteDto;
+import vstu.isd.notebin.dto.NotePreviewDto;
 import vstu.isd.notebin.dto.NoteViewResponseDto;
 import vstu.isd.notebin.dto.ViewAnalyticsDto;
+import vstu.isd.notebin.entity.NoteCacheable;
 import vstu.isd.notebin.entity.ViewNote;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static vstu.isd.notebin.util.UtilFunc.durationAreEquals;
+import static vstu.isd.notebin.util.UtilFunc.localDateTimeAreEquals;
 
 public class TestAsserts {
     public static void assertNoteDtoEquals(NoteDto expected, NoteDto actual) {
@@ -24,11 +28,24 @@ public class TestAsserts {
         assertEquals(expected.getUserId(), actual.getUserId());
     }
 
+    public static void assertNoteCacheableEquals(NoteCacheable expected, NoteCacheable actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getUrl(), actual.getUrl());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getContent(), actual.getContent());
+        assertEquals(expected.isAvailable(), actual.isAvailable());
+        assertEquals(expected.getExpirationType(), actual.getExpirationType());
+        assertLocalDateTimeEquals(expected.getCreatedAt(), actual.getCreatedAt());
+        assertLocalDateTimeEquals(expected.getExpirationFrom(), actual.getExpirationFrom());
+        assertDurationEquals(expected.getExpirationPeriod(), actual.getExpirationPeriod());
+        assertEquals(expected.getUserId(), actual.getUserId());
+    }
+
     private static void assertLocalDateTimeEquals(LocalDateTime expected, LocalDateTime actual) {
         if (expected != null && actual != null) {
-            assertTrue(Duration.between(expected, actual).getSeconds() < 1);
+            assertTrue(localDateTimeAreEquals(expected, actual));
         } else if (expected == null && actual == null) {
-            assertEquals(expected, actual);
+            assertTrue(true);
         } else {
             fail();
         }
@@ -36,9 +53,9 @@ public class TestAsserts {
 
     private static void assertDurationEquals(Duration expected, Duration actual) {
         if (expected != null && actual != null) {
-            assertTrue(expected.minus(actual).getSeconds() < 1);
+            assertTrue(durationAreEquals(expected, actual));
         } else if (expected == null && actual == null) {
-            assertEquals(expected, actual);
+            assertTrue(true);
         } else {
             fail();
         }
@@ -60,5 +77,12 @@ public class TestAsserts {
         assertEquals(expected.getNoteId(), actual.getNoteId());
         assertEquals(expected.getUserId(), actual.getUserId());
         assertLocalDateTimeEquals(expected.getViewedAt(), actual.getViewedAt());
-    };
+    }
+
+    public static void assertNotePreviewDtoEquals(NotePreviewDto expected, NotePreviewDto actual) {
+        assertEquals(expected.getUrl(), actual.getUrl());
+        assertEquals(expected.getExpirationType(), actual.getExpirationType());
+        assertLocalDateTimeEquals(expected.getExpirationFrom(), actual.getExpirationFrom());
+        assertDurationEquals(expected.getExpirationPeriod(), actual.getExpirationPeriod());
+    }
 }
