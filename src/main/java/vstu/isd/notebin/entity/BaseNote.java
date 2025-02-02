66d906828a5c546a695447744db25c70b4580301
sauce-chili <saucechilidev@gmail.com>
2025-02-02 03:59:51 +0300

    feat: added usage common utils function

--- src/main/java/vstu/isd/notebin/entity/BaseNote.java
+++ src/main/java/vstu/isd/notebin/entity/BaseNote.java
@@ -4,36 +4,47 @@ import lombok.ToString;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Objects;
+
+import static vstu.isd.notebin.util.UtilFunc.durationAreEquals;
+import static vstu.isd.notebin.util.UtilFunc.localDateTimeAreEquals;
 
 @ToString
 public abstract class BaseNote {
     abstract public Long getId();
+
     abstract public void setId(Long id);
 
     abstract public String getTitle();
+
     abstract public void setTitle(String title);
 
     abstract public String getContent();
+
     abstract public void setContent(String content);
 
     abstract public LocalDateTime getCreatedAt();
+
     abstract public void setCreatedAt(LocalDateTime createdAt);
 
     abstract public String getUrl();
+
     abstract public void setUrl(String url);
 
     abstract public ExpirationType getExpirationType();
+
     abstract public void setExpirationType(ExpirationType expirationType);
 
     abstract public Duration getExpirationPeriod();
+
     abstract public void setExpirationPeriod(Duration expirationPeriod);
 
     abstract public LocalDateTime getExpirationFrom();
+
     abstract public void setExpirationFrom(LocalDateTime expirationFrom);
 
     abstract public Long getUserId();
+
     abstract public void setUserId(Long userId);
 
     public boolean isNoteOwner(Long userId) {
@@ -65,44 +76,21 @@ public abstract class BaseNote {
         return getExpirationFrom().plus(getExpirationPeriod());
     }
 
-
     @Override
-    public boolean equals(Object obj) {
-        if (obj == null) return false;
-        if (this == obj) return true;
-        if (!(obj instanceof BaseNote other)) return false;
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (this == other) return true;
+        if (!(other instanceof BaseNote o)) return false;
 
-        if (!getId().equals(other.getId())) return false;
-        if (!getTitle().equals(other.getTitle())) return false;
-        if (!getContent().equals(other.getContent())) return false;
-        if (!localDateTimeAreEquals(getCreatedAt(), other.getCreatedAt())) return false;
-        if (!getUrl().equals(other.getUrl())) return false;
-        if (getExpirationType() != other.getExpirationType()) return false;
-        if (!durationAreEquals(getExpirationPeriod(), other.getExpirationPeriod())) return false;
-        if (!localDateTimeAreEquals(getExpirationFrom(), other.getExpirationFrom())) return false;
-        if (!Objects.equals(getUserId(), other.getUserId())) return false;
-        return isAvailable() == other.isAvailable();
-    }
-
-    // only second or microseconds
-    private final static ChronoUnit PRECISION_UNIT = ChronoUnit.SECONDS;
-    private final static long TEMP_AMOUNT_PRECISION = 1;
-
-    private static boolean localDateTimeAreEquals(LocalDateTime first, LocalDateTime second) {
-        if (first == second) {
-            return true;
-        } else if (first != null && second != null) {
-            return Duration.between(first, second).get(PRECISION_UNIT) < TEMP_AMOUNT_PRECISION;
-        }
-        return false;
-    }
-
-    private static boolean durationAreEquals(Duration first, Duration second) {
-        if (first == second) {
-            return true;
-        } else if (first != null && second != null) {
-            return first.minus(second).get(PRECISION_UNIT) < TEMP_AMOUNT_PRECISION;
-        }
-        return false;
+        if (!Objects.equals(getId(), o.getId())) return false;
+        if (!Objects.equals(getTitle(), o.getTitle())) return false;
+        if (!Objects.equals(getContent(),o.getContent())) return false;
+        if (!localDateTimeAreEquals(getCreatedAt(), o.getCreatedAt())) return false;
+        if (!Objects.equals(getUrl(), o.getUrl())) return false;
+        if (getExpirationType() != o.getExpirationType()) return false;
+        if (!durationAreEquals(getExpirationPeriod(), o.getExpirationPeriod())) return false;
+        if (!localDateTimeAreEquals(getExpirationFrom(), o.getExpirationFrom())) return false;
+        if (!Objects.equals(getUserId(), o.getUserId())) return false;
+        return Objects.equals(isAvailable(), o.isAvailable());
     }
 }
